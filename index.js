f7e2743a8f9bb50629fa34fc29f312c4bd2f8e8f
Nuno Faria <nunofaria11@gmail.com>
2016-05-03 14:07:33 +0100

    post webhook

--- index.js
+++ index.js
@@ -8,11 +8,26 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-app.get('/webhook', function (req, res) {
+app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] === FB_WEB_HOOK) {
         res.send(req.query['hub.challenge']);
     }
     res.send('Error, wrong validation token');
+});
+
+app.post('/webhook/', function (req, res) {
+    var messaging_events, i, event, sender, text;
+    messaging_events = req.body.entry[0].messaging;
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i];
+        sender = event.sender.id;
+        if (event.message && event.message.text) {
+            text = event.message.text;
+            // Handle a text message from this sender
+            console.log('Received message:', text);
+        }
+    }
+    res.sendStatus(200);
 });
 
 
